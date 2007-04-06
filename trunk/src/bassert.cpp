c07560320ac44444bef4c4e95283626882082f55
tomasz.huczek <tomasz.huczek@72fc70fc-632d-0410-85fa-ab81a001da6a>
2007-04-06 21:47:35 +0000

    Dołączone nowe pliki

---
+++ trunk/src/bassert.cpp
@@ -0,0 +1,58 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by                                       *
+ *                                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "butils.h"
+#include "bassert.h"
+
+bException::bException( const char * ifile, int iline, const char * iexpr, const char * imsg )
+    : line(iline)
+{
+    file = bUtils::scpy( ifile );
+    expr = bUtils::scpy( iexpr );
+    msg = bUtils::scpy( imsg );
+}
+
+bException::~bException()
+{
+    delete [] file;
+    delete [] expr;
+    delete [] msg;
+}
+
+int bException::get_line()
+{
+    return line;
+}
+
+const char * bException::get_msg()
+{
+    return msg;
+}
+
+const char * bException::get_expr()
+{
+    return expr;
+}
+
+const char * bException::get_file()
+{
+    return file;
+}
+
+const char * bException::format()
+{
+    static char buffer[1024] = "";
+    
+    sprintf( buffer, "Exception occured in: %s\nat line: %d\nin expression: %s\n\n%s", 
+           file, line, expr, msg?msg:"" );
+    
+    return buffer;
+}
---
+++ trunk/src/butils.h
@@ -0,0 +1,28 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by                                       *
+ *                                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef BUTILSBUTILS_H
+#define BUTILSBUTILS_H
+
+namespace bUtils {
+
+/**
+Various utilities
+
+	@author Tomasz Huczek & Andrzej Jasiñski <thuczek@gmail.com>
+*/
+
+/// string copy - should be released by `delete []'
+char * scpy( const char * src );
+
+}
+
+#endif
