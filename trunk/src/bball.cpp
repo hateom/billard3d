5af375308a351626ae1f9021c5d59a44c109101c
tomasz.huczek <tomasz.huczek@72fc70fc-632d-0410-85fa-ab81a001da6a>
2007-05-11 21:32:44 +0000

    + back to previous rotation

--- trunk/src/bball.cpp
+++ trunk/src/bball.cpp
@@ -53,12 +53,10 @@ bBall::~bBall()
 void bBall::draw()
 {
     double mat[16];
-    bQuaternion q, q1(1.0,0.0,0.0,(phi.x*3.1415*2.0)/180.0), q2(0.0,0.0,1.0,(phi.y*3.1415*2.0)/180.0);
+    bQuaternion q, q1(-1.0,0.0,0.0,(phi.x*3.1415*2.0)/180.0), q2(0.0,0.0,-1.0,(phi.y*3.1415*2.0)/180.0);
     q = q1 * q2;
     q.create_matrix(mat);
     glPushMatrix();
-//		glRotated( (phi.y*3.1415*2.0)/180.0, 0.0f, 0.0f, 1.0f );
-//		glRotated( (phi.x*3.1415*2.0)/180.0, 1.0f, 0.0f, 0.0f );
         glMultMatrixd(mat);
 		glTranslated( pos.x, 0.0, pos.y );
 		glColor3f( r, g, b );
