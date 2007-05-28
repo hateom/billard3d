0d929853f7382146bec562c61954e3a94c47225f
tomasz.huczek <tomasz.huczek@72fc70fc-632d-0410-85fa-ab81a001da6a>
2007-05-28 19:25:43 +0000

    + splash

--- trunk/src/layers/bsyncvideolayer.cpp
+++ trunk/src/layers/bsyncvideolayer.cpp
@@ -12,6 +12,8 @@
 #include "bsyncvideolayer.h"
 #include "bfontmgr.h"
 #include "bgl.h"
+#include "bsystem.h"
+#include "bconfigreader.h"
 
 bSyncVideoLayer::bSyncVideoLayer()
  : bVideoLayer()
@@ -25,9 +27,18 @@ bSyncVideoLayer::~bSyncVideoLayer()
 
 void bSyncVideoLayer::draw()
 {
+    int w = GetConfig.screen_w, h = GetConfig.screen_h;
     glPushMatrix();
+        glColor4d( 0.1, 0.1, 0.2, 1.0 );
+        bSystem::video_sys.set_matrix_2d();
+        glBegin( GL_TRIANGLE_STRIP );
+            glVertex2i( 0, 0 );
+            glVertex2i( 0, 600 );
+            glVertex2i( 800, 0 );
+            glVertex2i( 800, 600 );
+        glEnd();
         glColor4d( 1.0, 1.0, 1.0, 1.0 );
-        PRINTB( 200, 300, B_FONT_LIGHT, "...SYNCHRONIZING FPS TIMER..." );
+        PRINTB( 250, 300, B_FONT_LIGHT, "...SYNCHRONIZING FPS TIMER..." );
     glPopMatrix();
 }
 
